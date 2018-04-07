f5a7a6d742a78acd01789faa8d0bd2eef43a9abc
yadushini <yadushi44.cool@gmail.com>
2018-04-08 01:05:01 +1200

    deleted files for restruction of project

--- client/store/index.js
+++
@@ -1,7 +0,0 @@
-import Vuex from 'vuex'
-
-const store = {
-  state: {}
-}
-
-export default () => new Vuex.Store(store)
--- nuxt.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mode: 'universal',
-  buildDir: '.build',
-  srcDir: 'client/',
-  loading: false
-}
