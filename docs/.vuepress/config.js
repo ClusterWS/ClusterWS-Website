83bae753e73472c38959cbc44c2d5f28bf42093c
Dmitrii Goriunov <goriunovd@gmail.com>
2018-10-12 22:48:55 +1300

    Added GA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: "ClusterWS",
   description: "Lightweight, fast and powerful framework for building scalable WebSocket applications in Node.js",
+  ga: "UA-127398674-1",
   themeConfig: {
     nav: [
       { text: 'GitHub', link: 'https://github.com/ClusterWS/ClusterWS' }
